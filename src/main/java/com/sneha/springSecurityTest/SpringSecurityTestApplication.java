4a64be09d1c98976a41714c1aaae1370789aad21
Sneha6080 <snehasomnathe2017@gmail.com>
2024-11-06 21:00:12 +0530

    Added changes

--- src/main/java/com/sneha/springSecurityTest/SpringSecurityTestApplication.java
+++ src/main/java/com/sneha/springSecurityTest/SpringSecurityTestApplication.java
@@ -2,8 +2,10 @@ package com.sneha.springSecurityTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages  = {"com.sneha.springSecurityTest.config"})
 public class SpringSecurityTestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sneha/springSecurityTest/config/SecurityConfig.java
+++ src/main/java/com/sneha/springSecurityTest/config/SecurityConfig.java
@@ -2,19 +2,54 @@ package com.sneha.springSecurityTest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(){
+		
+		UserDetails normalUser = User.withUsername("Sneha")
+				.password(passwordEncoder().encode("password"))
+				.roles("NORMAL")
+				.build();
+		
+		UserDetails adminUser = User.withUsername("Sneha1")
+				.password(passwordEncoder().encode("password"))
+				.roles("ADMIN")
+				.build();
+		
+		InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(normalUser, adminUser);
+		return inMemoryUserDetailsManager;
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		
 		httpSecurity.csrf().disable()
 		.authorizeHttpRequests()
-		.requestMatchers("/home/public")
-		.permitAll()
+//		.requestMatchers("home/normal")
+//		.hasRole("NORMAL")
+//		.requestMatchers("home/admin")
+//		.hasRole("ADMIN")
+//		.requestMatchers("/home/public")
+//		.permitAll()
 		.anyRequest()
 		.authenticated()
 		.and()
