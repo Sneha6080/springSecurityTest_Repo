f815e364059a1b981ac243884a64ba73ecef0b6f
Sneha6080 <snehasomnathe2017@gmail.com>
2024-11-06 21:06:25 +0530

    Added changes

--- src/main/java/com/sneha/springSecurityTest/SpringSecurityTestApplication.java
+++ src/main/java/com/sneha/springSecurityTest/SpringSecurityTestApplication.java
@@ -2,10 +2,8 @@ package com.sneha.springSecurityTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages  = {"com.sneha.springSecurityTest.config"})
 public class SpringSecurityTestApplication {
 
 	public static void main(String[] args) {
