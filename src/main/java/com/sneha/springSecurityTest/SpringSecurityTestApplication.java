6b05734e713ede907c323265e95fdc0a3ec56425
Sneha6080 <snehasomnathe2017@gmail.com>
2024-11-06 18:40:13 +0530

    Project Added

---
+++ src/main/java/com/sneha/springSecurityTest/SpringSecurityTestApplication.java
@@ -0,0 +1,13 @@
+package com.sneha.springSecurityTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sneha/springSecurityTest/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.springSecurityTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
